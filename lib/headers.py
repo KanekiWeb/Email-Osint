79489b38f61e48f05e07bf3907d963c5c5c9c76f
Kaneki <72992708+KanekiX2@users.noreply.github.com>
2021-03-12 18:16:53 +0100

    Delete lib directory

--- lib/headers.py
+++
@@ -1,9 +0,0 @@
-import colorama, os
-from colorama import Fore
-
-def Header(text):
-	os.system('title Email Osint ^| '+text)
-	print("")
-	print("-------------------------------")
-	print(f"{Fore.GREEN}>{Fore.RESET}  "+text)
-	print("-------------------------------")
