5faf750d9709b23d50a7ca45a4154dcd3e9eaafe
Kaneki <72992708+KanekiX2@users.noreply.github.com>
2021-03-24 18:24:37 +0100

    Update Holele_Module.py

--- modules/Holele_Module.py
+++ modules/Holele_Module.py
@@ -38,130 +38,134 @@ from holehe.modules.shopping.amazon import *
 from holehe.modules.forum.cracked_to import *
 
 def account_detect(email):
-	list_acc = []
-	async def acc_amazon():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await amazon(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+	try:
+		list_acc = []
+		async def acc_amazon():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await amazon(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_ebay():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await ebay(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_ebay():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await ebay(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_blablacar():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await blablacar(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_blablacar():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await blablacar(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_buymeacoffee():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await buymeacoffee(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_buymeacoffee():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await buymeacoffee(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_cracked_to():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await cracked_to(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_cracked_to():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await cracked_to(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_github():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await github(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_github():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await github(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_snapchat():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await snapchat(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_snapchat():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await snapchat(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_discord():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await discord(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_discord():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await discord(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_instagram():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await instagram(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_instagram():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await instagram(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_twitter():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await twitter(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_twitter():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await twitter(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_spotify():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await spotify(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_spotify():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await spotify(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_redutbe():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await redtube(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_redutbe():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await redtube(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_pornhub():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await pornhub(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_pornhub():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await pornhub(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	async def acc_xvideos():
-	    out = []
-	    client = httpx.AsyncClient()
-	    await xvideos(email, client, out)
-	    list_acc.append(out)
-	    await client.aclose()
+		async def acc_xvideos():
+		    out = []
+		    client = httpx.AsyncClient()
+		    await xvideos(email, client, out)
+		    list_acc.append(out)
+		    await client.aclose()
 
-	trio.run(acc_amazon)
-	trio.run(acc_ebay)
-	trio.run(acc_blablacar)
-	trio.run(acc_buymeacoffee)
-	trio.run(acc_cracked_to)
-	trio.run(acc_github)
-	trio.run(acc_snapchat)
-	trio.run(acc_discord)
-	trio.run(acc_instagram)
-	trio.run(acc_twitter)
-	trio.run(acc_spotify)
-	trio.run(acc_redutbe)
-	trio.run(acc_pornhub)
-	trio.run(acc_xvideos)
+		trio.run(acc_amazon)
+		trio.run(acc_ebay)
+		trio.run(acc_blablacar)
+		trio.run(acc_buymeacoffee)
+		trio.run(acc_cracked_to)
+		trio.run(acc_github)
+		trio.run(acc_snapchat)
+		trio.run(acc_discord)
+		trio.run(acc_instagram)
+		trio.run(acc_twitter)
+		trio.run(acc_spotify)
+		trio.run(acc_redutbe)
+		trio.run(acc_pornhub)
+		trio.run(acc_xvideos)
 
-	accounts_founds = []
+		accounts_founds = []
 
-	for i in list_acc:
-	    try:
-	        if i[0]['exists'] == True:
-	            accounts_founds.append(i[0]['name'])
-	    except:
-	        pass
+		for i in list_acc:
+		    try:
+		        if i[0]['exists'] == True:
+		            accounts_founds.append(i[0]['name'])
+		    except:
+		        pass
 
-	a = accounts_founds
-	founds = str(a).replace('[','').replace(']','').replace("'","").replace(', ','\n- ').upper()
-	
-	print("- "+founds)
+		a = accounts_founds
+		founds = str(a).replace('[','').replace(']','').replace("'","").replace(', ','\n- ').upper()
+		
+		print("- "+founds)
+
+	except:
+		return None
